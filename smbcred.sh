badb21af21faf7347c715bfe8a62ccd26a396256
sa-savi <savi.toy@amkcambodia.com>
2025-06-05 16:29:59 +0700

    Update New

--- smbcred.sh
+++ smbcred.sh
@@ -17,13 +17,13 @@ domain=$domain
 CRED
         chmod 600 "$cred_file"
         zenity --info --text="✅ SMB credentials saved successfully."
-        echo "✅ SMB credentials saved to $cred_file"
+        echo "✅ SMB credentials saved"
     else
         zenity --error --text="❌ Missing credentials. Login failed."
         echo "❌ Missing username or password. Aborted."
     fi
 else
-    echo "✅ Credentials already exist in $cred_file"
+    echo "✅ Credentials already exist"
 fi
 EOF
 chmod +x /bin/smbcred.sh
