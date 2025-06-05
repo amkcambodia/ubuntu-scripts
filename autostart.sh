f9f52a60b9aba0a610dc1f5281afb900cd42ee02
sa-savi <savi.toy@amkcambodia.com>
2025-06-05 17:23:25 +0700

    Update New

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.config/autostart
+
+cat <<EOF > ~/.config/autostart/smbcred.desktop
+[Desktop Entry]
+Type=Application
+Exec=/bin/smbcred.sh
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=SMB Credentials Prompt
+Comment=Run SMB credentials script after login
+EOF
