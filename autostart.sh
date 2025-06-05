327aa10d64e73f44da2867a01c99cff281705a17
sa-savi <savi.toy@amkcambodia.com>
2025-06-05 17:44:05 +0700

    Update New

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,11 @@
 cat <<EOF > /etc/xdg/autostart/smbcred.desktop
 [Desktop Entry]
 Type=Application
-Exec=/bin/smbcred.sh
+Exec=/bin/smbcred.sh >> /tmp/smbcred.log 2>&1
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
 Name=SMB Credentials Prompt
 Comment=Run SMB credentials script after login
 EOF
-chmod +x /etc/xdg/autostart/smbcred.desktop
+chmod 644 /etc/xdg/autostart/smbcred.desktop
