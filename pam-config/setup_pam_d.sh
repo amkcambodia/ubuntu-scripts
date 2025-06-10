6bada33e17ff086cf79b2d4edea12eb3233b998f
sa-savi <savi.toy@amkcambodia.com>
2025-06-10 16:41:08 +0700

    update

--- pam-config/setup_pam_d.sh
+++ pam-config/setup_pam_d.sh
@@ -5,5 +5,14 @@
 sudo cp ./pam-config/template/fix_dconf_profile.sh /usr/local/bin/fix_dconf_profile.sh
 sudo chmod 755 /usr/local/bin/fix_dconf_profile.sh && sudo chmod +x /usr/local/bin/fix_dconf_profile.sh
 
+# ----------------------------------------------------------------
+
 # Configure PAM User can run mount drive script
 sudo ./pam-config/tasks/setup_dfs_sudo_access.sh
+
+# ----------------------------------------------------------------
+
+# Configure Ignore the loca passwd user
+sudo ./pam-config/template/fix_auth_pam.sh
+
+# ----------------------------------------------------------------
